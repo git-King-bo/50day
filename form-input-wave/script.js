bff297a3fc21202bb59c7eceb1cb907b546daa54
wangbo <wangbo@juxingtech.cn>
2022-12-14 11:50:37 +0800

    fix:bug 修复

--- form-input-wave/script.js
+++ form-input-wave/script.js
@@ -1,8 +1,19 @@
 const labels = document.querySelectorAll('.form-control label')
-
-labels.forEach(label => {
-    label.innerHTML = label.innerText
-        .split('')
-        .map((letter, idx) => `<span style="transition-delay:${idx * 50}ms">${letter}</span>`)
-        .join('')
+labels.forEach((label) => {
+  label.innerHTML = label.innerText
+    .split('')
+    .map((letter, idx) => `<span style="transition-delay:${idx * 50}ms">${letter}</span>`)
+    .join('')
+})
+var btn = document.getElementById('btn')
+var inva = document.getElementsByName('username')[0]
+inva.addEventListener('input', (e) => {
+  if (e.target.value.length > 0) {
+    inva.className = 'foc'
+  } else {
+    inva.classList.remove('foc')
+  }
+})
+btn.addEventListener('click', () => {
+  console.log(document.getElementsByName('username')[0].value, '---')
 })
